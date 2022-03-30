a591b7bcb5d2a4f49f1f3d93c62cb0faa7c34fff
Bhavya <101401527+Bhavyajha00@users.noreply.github.com>
2022-03-30 15:20:13 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,11 @@
+public class wrapper {
+    public static void main(String [] args){
+        Integer myInt = 5;
+        Double mydouble = 8.64;
+        Character mychar = 'p';
+        System.out.println(myInt);
+        System.out.println(mydouble);
+        System.out.println(mychar);
+     } 
+    
+}
---
+++ wrapper2.java
@@ -0,0 +1,7 @@
+public class wrapper2 {
+    public static void main(String[] args) {
+      Integer myInt = 7009543;
+      String myString = myInt.toString();
+      System.out.println(myString.length());
+    }
+  }
